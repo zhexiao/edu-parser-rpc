a7a106f848f249519a4fbfcc80d4d9c5b3689d8f
zhexiao <zhexiao@163.com>
2020-01-14 16:56:10 +0800

    新增excel测试

--- excel/srv/utils_test.go
+++ excel/srv/utils_test.go
@@ -1,1 +1,25 @@
 package srv
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+func initExcel() *CT_Excel {
+	return &CT_Excel{}
+}
+
+func TestWordPaper(t *testing.T) {
+	fileBytes, err := ioutil.ReadFile("../_testdoc/paper_20190702.xlsx")
+	if err != nil {
+		t.Errorf("文件读取失败 err=%s", err)
+	}
+
+	jsonData, err := initExcel().paper(fileBytes)
+	if err != nil {
+		t.Errorf("文件解析失败 err=%s", err)
+	}
+
+	log.Println(jsonData)
+}
