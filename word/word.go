9b5705756ec039715cea98ada17936f5ed0f9e8e
zhexiao <zhexiao@163.com>
2020-01-14 11:32:12 +0800

    新增word rpc服务

--- word/word.go
+++ word/word.go
@@ -4,40 +4,38 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/zhexiao/edu-parser-proto/basepb"
 	"github.com/zhexiao/office-parser/word"
-	"log"
 )
 
 type CT_Word struct {
 }
 
 func (c *CT_Word) ParserPaper(ctx context.Context, req *basepb.Request, resp *basepb.Response) error {
-	log.Println("word paper parser ...")
-
 	fileBytes := req.Body
 	data := word.ConvertPaperFromData(bytes.NewReader(fileBytes), int64(len(fileBytes)))
 
 	jsonBytes, err := json.Marshal(data)
 	if err != nil {
-		log.Panicf("json转换失败: %s", err)
+		resp.Data = fmt.Sprintf("json转换失败: %s", err)
+		return nil
 	}
-	resp.Data = string(jsonBytes)
 
+	resp.Data = string(jsonBytes)
 	return nil
 }
 
 func (c *CT_Word) ParserQuestion(ctx context.Context, req *basepb.Request, resp *basepb.Response) error {
-	log.Println("word question parser ...")
 	fileBytes := req.Body
-
 	data := word.ConvertFromData(bytes.NewReader(fileBytes), int64(len(fileBytes)))
 
 	jsonBytes, err := json.Marshal(data)
 	if err != nil {
-		log.Panicf("json转换失败: %s", err)
+		resp.Data = fmt.Sprintf("json转换失败: %s", err)
+		return nil
 	}
-	resp.Data = string(jsonBytes)
 
+	resp.Data = string(jsonBytes)
 	return nil
 }
