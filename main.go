27baba14451a4e122479dc7dffe398887e2dfc65
zhexiao <zhexiao@163.com>
2020-01-14 10:57:02 +0800

    新增yaml支持

--- main.go
+++ main.go
@@ -7,8 +7,35 @@ import (
 	"github.com/micro/go-plugins/registry/etcdv3"
 	"github.com/zhexiao/edu-parser-proto/wordpb"
 	"github.com/zhexiao/edu-parser-rpc/word"
+	"github.com/zhexiao/office-parser/utils"
 	"time"
 )
+
+func init() {
+	//读取配置文件
+	yamlSettings := ReadYaml()
+
+	//初始化七牛的配置
+	utils.OfficeParserQiniuCfg = &utils.Qiniu{
+		//七牛key
+		AccessKey: yamlSettings.Qiniu.AccessKey,
+		//七牛secret
+		SecretKey: yamlSettings.Qiniu.SecretKey,
+		//七牛存储的bucket
+		Bucket: yamlSettings.Qiniu.Bucket,
+		//所属区域
+		Zone: yamlSettings.Qiniu.Zone,
+		//访问的域名地址
+		Domain: yamlSettings.Qiniu.Domain,
+		//路径
+		UploadPrefix: yamlSettings.Qiniu.UploadPrefix,
+	}
+
+	//初始化WMF的配置
+	utils.WmfConfiguration = &utils.CT_WmfCfg{
+		Uri: yamlSettings.Wmf.Uri,
+	}
+}
 
 func main() {
 	reg := etcdv3.NewRegistry(func(options *registry.Options) {
---
+++ yaml.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/go-yaml/yaml"
+	"io/ioutil"
+	"log"
+)
+
+type CT_YamlSettings struct {
+	Proj struct {
+		Release bool `yaml:"release"`
+	}
+
+	Qiniu struct {
+		AccessKey    string `yaml:"accessKey"`
+		SecretKey    string `yaml:"secretKey"`
+		Bucket       string `yaml:"bucket"`
+		Zone         string `yaml:"zone"`
+		Domain       string `yaml:"domain"`
+		UploadPrefix string `yaml:"uploadPrefix"`
+	}
+
+	Wmf struct {
+		Uri string `yaml:"uri"`
+	}
+}
+
+func ReadYaml() *CT_YamlSettings {
+	//读取配置文件
+	yamlSettings := new(CT_YamlSettings)
+
+	yamlFile, err := ioutil.ReadFile("settings.yaml")
+	if err != nil {
+		log.Panicf("配置文件读取失败,err=%s", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, yamlSettings)
+	if err != nil {
+		log.Fatalf("Yaml解析失败: err=%s", err)
+	}
+
+	return yamlSettings
+}
