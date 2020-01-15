132c7a40b9e233d7b7bb98e8a7d8c07fb26a5369
a342058040 <342058040@qq.com>
2020-01-15 03:40:51 +0000

    excel func

--- excel/srv/utils_test.go
+++ excel/srv/utils_test.go
@@ -7,16 +7,73 @@ import (
 )
 
 func initExcel() *CT_Excel {
-	return &CT_Excel{}
+	return NewCT_Excel()
 }
 
-func TestWordPaper(t *testing.T) {
+func TestExcelPaper(t *testing.T) {
 	fileBytes, err := ioutil.ReadFile("../_testdoc/paper_20190702.xlsx")
 	if err != nil {
 		t.Errorf("文件读取失败 err=%s", err)
 	}
 
 	jsonData, err := initExcel().paper(fileBytes)
+	if err != nil {
+		t.Errorf("文件解析失败 err=%s", err)
+	}
+
+	log.Println(jsonData)
+}
+
+
+func TestExcelOutline(t *testing.T) {
+	fileBytes, err := ioutil.ReadFile("../_testdoc/outline-20190129.xlsx")
+	if err != nil {
+		t.Errorf("文件读取失败 err=%s", err)
+	}
+
+	jsonData, err := initExcel().outline(fileBytes)
+	if err != nil {
+		t.Errorf("文件解析失败 err=%s", err)
+	}
+
+	log.Println(jsonData)
+}
+
+func TestExcelBook(t *testing.T) {
+	fileBytes, err := ioutil.ReadFile("../_testdoc/book.xlsx")
+	if err != nil {
+		t.Errorf("文件读取失败 err=%s", err)
+	}
+
+	jsonData, err := initExcel().book(fileBytes)
+	if err != nil {
+		t.Errorf("文件解析失败 err=%s", err)
+	}
+
+	log.Println(jsonData)
+}
+
+func TestExcelCognitionMap(t *testing.T) {
+	fileBytes, err := ioutil.ReadFile("../_testdoc/cognition-map-20190129.xlsx")
+	if err != nil {
+		t.Errorf("文件读取失败 err=%s", err)
+	}
+
+	jsonData, err := initExcel().cognition_map(fileBytes)
+	if err != nil {
+		t.Errorf("文件解析失败 err=%s", err)
+	}
+
+	log.Println(jsonData)
+}
+
+func TestExcelCognitionSp(t *testing.T) {
+	fileBytes, err := ioutil.ReadFile("../_testdoc/cognition-sp-20190301.xlsx")
+	if err != nil {
+		t.Errorf("文件读取失败 err=%s", err)
+	}
+
+	jsonData, err := initExcel().cognition_sp(fileBytes)
 	if err != nil {
 		t.Errorf("文件解析失败 err=%s", err)
 	}
--- excel/srv/utlis.go
+++ excel/srv/utlis.go
@@ -25,3 +25,48 @@ func (c *CT_Excel) paper(fileData []byte) (string, error) {
 
 	return string(jsonBytes), nil
 }
+
+//读取 excel outline 数据
+func (c *CT_Excel) outline(fileData []byte) (string, error) {
+	data := excel.ConvertFromData(bytes.NewReader(fileData), int64(len(fileData)), "outline")
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return "", base.NewEpError(1, fmt.Sprintf("Excel教材目录 json转换失败: %s", err))
+	}
+
+	return string(jsonBytes), nil
+}
+
+
+//读取 excel book 数据
+func (c *CT_Excel) book(fileData []byte) (string, error) {
+	data := excel.ConvertFromData(bytes.NewReader(fileData), int64(len(fileData)), "book")
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return "", base.NewEpError(1, fmt.Sprintf("Excel书籍 json转换失败: %s", err))
+	}
+
+	return string(jsonBytes), nil
+}
+
+//读取 excel cognition_map 数据
+func (c *CT_Excel) cognition_map(fileData []byte) (string, error) {
+	data := excel.ConvertFromData(bytes.NewReader(fileData), int64(len(fileData)), "cognition_map")
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return "", base.NewEpError(1, fmt.Sprintf("Excel普适认知点 json转换失败: %s", err))
+	}
+
+	return string(jsonBytes), nil
+}
+
+//读取 excel cognition_sp 数据
+func (c *CT_Excel) cognition_sp(fileData []byte) (string, error) {
+	data := excel.ConvertFromData(bytes.NewReader(fileData), int64(len(fileData)), "cognition_sp")
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return "", base.NewEpError(1, fmt.Sprintf("Excel特异认知点 json转换失败: %s", err))
+	}
+
+	return string(jsonBytes), nil
+}
